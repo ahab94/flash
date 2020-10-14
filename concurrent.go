7e2da3519c7b3e896fe32e5bf449010883abe661
ahab94 <abdshah94@gmail.com>
2020-10-14 14:19:18 +0500

    add success/fail handler options

--- concurrent.go
+++ concurrent.go
@@ -11,19 +11,43 @@ import (
 // Concurrent is an executor for concurrent executions
 type Concurrent struct {
 	executor
-	engine *engine.Engine
-	block  bool
+	engine         *engine.Engine
+	block          bool
+	successHandler func()
+	failHandler    func(err error)
 }
 
+type ConcurrentOption func(*Concurrent)
+
 // NewConcurrent - initializes concurrent executor; if completionBlock=true, it will block main routine until all tasks completed
-func NewConcurrent(ctx context.Context, engine *engine.Engine, completionBlock bool) *Concurrent {
-	return &Concurrent{
+func NewConcurrent(ctx context.Context, engine *engine.Engine, completionBlock bool, opts ...ConcurrentOption) *Concurrent {
+	con := &Concurrent{
 		executor: executor{
 			id:  fmt.Sprintf("%s-%s", "concurrent", uuid.NewV4().String()),
 			ctx: ctx,
 		},
 		engine: engine,
 		block:  completionBlock,
+	}
+
+	for _, opt := range opts {
+		opt(con)
+	}
+
+	return con
+}
+
+// ConcurrentFailHandler - inits fail handler
+func ConcurrentFailHandler(fail func(err error)) ConcurrentOption {
+	return func(s *Concurrent) {
+		s.failHandler = fail
+	}
+}
+
+// ConcurrentSuccessHandler - inits success handler
+func ConcurrentSuccessHandler(success func()) ConcurrentOption {
+	return func(c *Concurrent) {
+		c.successHandler = success
 	}
 }
 
@@ -50,5 +74,21 @@ func (c *Concurrent) executeDispatch() {
 		for _, done := range doneChans {
 			<-done
 		}
+	}
+}
+
+// OnSuccess - handles completion callback
+func (c *Concurrent) OnSuccess() {
+	c.executor.OnSuccess()
+	if c.successHandler != nil {
+		c.successHandler()
+	}
+}
+
+// OnFailure - handles failure callback
+func (c *Concurrent) OnFailure(err error) {
+	c.executor.OnFailure(err)
+	if c.failHandler != nil {
+		c.failHandler(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.12
 require (
 	github.com/ahab94/engine v0.1.0
 	github.com/kr/pretty v0.1.0 // indirect
-	github.com/pkg/errors v0.8.1
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.4.2
 )
